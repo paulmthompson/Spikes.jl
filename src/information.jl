478af7ea5c382ecbe391aa8ca05ce7f08e66934b
Paul Thompson <pmt10@case.edu>
2015-09-28 23:18:48 -0400

    outlined information theory bias methods to add

---
+++ src/information.jl
@@ -0,0 +1,28 @@
+
+
+
+#=
+Bias correction techniques
+
+=#
+
+
+#=
+Quadratic extrapolation
+Strong et al 1998
+=#
+
+#=
+Panzeri-Treves (PT)
+Panzeri and Treves 1996
+=#
+
+#=
+Nemenman-Shafee-Bialek (NSB)
+Nemenman et al 2002
+=#
+
+#=
+James-Stein shrinkage
+Hausser and Strimmer 2008
+=#
